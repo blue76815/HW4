920163dc1b80063b5c757168724e12bea45812e2
blue <blue76815@gmail.com>
2021-05-09 15:53:19 +0800

    type1

--- fileperf.c
+++ fileperf.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 int main(int argc, char **argv)
 {
-    if(argc==4){
-        printf("argc=%d\n",argc);
-        printf("argv[0]=%s\n",argv[0]);
-        printf("argv[1]=%s\n",argv[1]);
-        printf("argv[2]=%s\n",argv[2]);
-        printf("argv[3]=%s\n",argv[3]);
+    // if(argc!=4){
+    //     printf("輸入參數不到3個\n");
+    // }
+    FILE* input;
+    printf("argv[1]=%s\r\n",argv[1]);
+    input = fopen(argv[1],"r");//開啟第一個輸入變數 source_file 
+    if(input==NULL)
+    {
+        printf("file %s not exist\r\n",argv[1]);
+        printf( "open failure\r\n" );
     }else{
-
-        printf("輸入參數不到3個\n");
+        printf("開檔成功\r\n");
+        fclose(input);//若前面fopen（input）失敗 則後面fclose(input);時 會出現 程式記憶體區段錯誤 (核心已傾印)
     }
+    
 	return 0;
 }
